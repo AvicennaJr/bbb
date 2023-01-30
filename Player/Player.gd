e334f07770f026e4dd3d38cc01f207b7df351cb5
AvicennaJr <justfuad@protonmail.com>
2023-01-31 01:11:28 +0300

    Change speed of player

--- Player/Player.gd
+++ Player/Player.gd
@@ -84,6 +84,8 @@ func get_input():
 		return
 	if Input.is_action_pressed("thrust"):
 		thrust = Vector2(engine_power, 0)
+	if Input.is_action_pressed("reverse"):
+		thrust = Vector2(-engine_power, 0)
 	if Input.is_action_pressed("rotate_right"):
 		rotation_dir += 1
 	if Input.is_action_pressed("rotate_left"):
