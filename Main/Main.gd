76f3e50aafa46364eb307b51276159c56d5e1d68
AvicennaJr <justfuad@protonmail.com>
2023-02-03 22:37:31 +0300

    Fix pause glitch

--- Main/Main.gd
+++ Main/Main.gd
@@ -19,17 +19,18 @@ func _input(event):
 	if event.is_action_pressed("pause"):
 		if not playing:
 			return
-	get_tree().paused = not get_tree().paused
-	if get_tree().paused:
-		$HUD/MessageLabel.text = "Paused"
-		$HUD/MessageLabel.show()
-	else:
-		$HUD/MessageLabel.text = ""
-		$HUD/MessageLabel.hide()
+		get_tree().paused = not get_tree().paused
+		if get_tree().paused:
+			$HUD/MessageLabel.text = "Paused"
+			$HUD/MessageLabel.show()
+		else:
+			$HUD/MessageLabel.text = ""
+			$HUD/MessageLabel.hide()
 
 func _process(delta):
 	if playing and $Rocks.get_child_count() == 0:
 		new_level()
+
 
 func _on_Player_shoot(bullet, pos, dir):
 	var b = bullet.instance()
